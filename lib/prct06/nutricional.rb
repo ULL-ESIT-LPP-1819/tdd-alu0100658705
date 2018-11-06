fbcbc3553df509add4acb502578f8dfff0720fbd
Pablo Bethencourt <alu0100658705@ull.edu.es>
2018-11-06 12:13:23 +0000

    TDD 1 con variable nombre

--- lib/prct06/nutricional.rb
+++ lib/prct06/nutricional.rb
@@ -1,2 +1,12 @@
 class Nutricional
+#	attr_reader :nombre
+
+#	def initialize(nombre)
+		@nombre = nombre
+#	end
+
+#	def Nombre
+		@nombre
+#	end
+
 end
--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -1,12 +1,22 @@
+require 'prct06/nutricional.rb'
 RSpec.describe Prct06 do
-  it "has a version number" do
-    expect(Prct06::VERSION).not_to be nil
-  end
+  	#it "has a version number" do
+    	#expect(Prct06::VERSION).not_to be nil
+  	#end
 
-  it "does something useful" do
-    expect(true).to eq(true)
-  end
+  	#it "does something useful" do
+    	#expect(true).to eq(true)
+  	#end
 
-  describe Nutricional do
-  end
+ 	#describe Nutricional do
+	before :each do
+		@galletas = Nutricional.new("Oreo")
+	end
+
+	describe "# almacenamiento del Nombre" do
+		it "Se almacena el nombre correctamente" do
+		expect(@galletas.Nombre).to eq("Oreo")
+		end
+	end
+
 end
