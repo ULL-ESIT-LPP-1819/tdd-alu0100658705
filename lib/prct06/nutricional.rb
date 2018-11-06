6da3b27fd3238e5f54208db951ef6b9c7693f618
Pablo Bethencourt <alu0100658705@ull.edu.es>
2018-11-06 16:10:41 +0000

    Modificación para valor energético

--- lib/prct06/nutricional.rb
+++ lib/prct06/nutricional.rb
@@ -1,8 +1,8 @@
 class Nutricional
-	attr_reader :nombre, :grasa, :grasa_saturada, :hidratos, :azucares, :proteinas, :sal, :valor_energético 
+	attr_reader :nombre, :grasa, :grasa_saturada, :hidratos, :azucares, :proteinas, :sal #:valor_energetico 
 
-	def initialize(nombre,grasa,grasa_saturada,hidratos,azucares,proteinas,sal,valor_energetico)
-		@nombre, @grasa, @grasa_saturada, @hidratos, @azucares, @proteinas, @sal, @valor_energetico  = nombre, grasa, grasa_saturada, hidratos, azucares, proteinas, sal, valor_energetico
+	def initialize(nombre,grasa,grasa_saturada,hidratos,azucares,proteinas,sal)
+		@nombre, @grasa, @grasa_saturada, @hidratos, @azucares, @proteinas, @sal  = nombre, grasa, grasa_saturada, hidratos, azucares, proteinas, sal
 	end
 
 	def Nombre
@@ -36,6 +36,35 @@ class Nutricional
 	def Valor_energetico
 	(@grasa*37)+(@hidratos*17)+(@proteinas*17)+(@sal*25)
 	end
+
+	producto = Nutricional.new("Bollicao",80,20,100,50,12,3);
+	 
+	puts "----------------------------------------------"
+	if  producto.grasa > 70
+	puts "Ha superado el valor de Ingesta de Referencia para las grasas" 
+	end
+
+	if producto.hidratos > 260
+        puts "Ha superado el valor de Ingesta de Referencia para los hidratos"
+        end
+
+	if  producto.azucares > 90 
+        puts "Ha superado el valor de Ingesta de Referencia para los azucares"
+        end
+	
+	if  producto.proteinas > 50
+        puts "Ha superado el valor de Ingesta de Referencia para las proteinas"
+        end
+	
+	if  producto.sal > 6
+        puts "Ha superado el valor de Ingesta de Referencia para la sal"
+        end
+
+	#if producto.valor_energetico > 8400
+	#puts "Ha superado el valor de Ingesta de Referencia para el valor energético"
+	#end
+
+
 end
 
 
