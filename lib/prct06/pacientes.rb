9908de34d0dfeaa57b766cd5024c18d97e79405e
Pablo Bethencourt <alu0100658705@ull.edu.es>
2018-11-20 10:17:33 +0000

    Añadidos los métodos para imc y clasificar

--- lib/prct06/pacientes.rb
+++ lib/prct06/pacientes.rb
@@ -14,4 +14,21 @@ class Pacientes < Individuo
 	s
 	
 	end
+
+	def imc
+	(@pseo/(@talla*@talla)).round(2)
+	end
+
+	def clasificar
+		if @head.value.imc < 30
+			return "No está en tratamiento"
+		elsif @head.value.imc <= 34.9
+			return "Tratamiento para obesidad tipo 1"
+		elsif @head.value.imc <= 39.9
+			return "Tratamiento para obesidad tipo 2"
+		else @head.value.imc > 40 
+			return "Tratamiento para obesidad tipo 3"
+		end
+	end
+
 end
