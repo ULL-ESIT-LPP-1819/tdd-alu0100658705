4f3060fb2e5165a708f7790e6e378d0f6f0d530f
Pablo Bethencourt <alu0100658705@ull.edu.es>
2018-12-11 21:44:40 +0000

    Método gasto energético basal y pruebas efecto termógeno

--- spec/prct10_spec.rb
+++ spec/prct10_spec.rb
@@ -1,4 +1,3 @@
-
 RSpec.describe Pacientes do
 	before :each do
 		@persona1=Pacientes.new("Pablo",26,1,70,1.77,[],[],[],[],[],[])
@@ -7,58 +6,63 @@ RSpec.describe Pacientes do
 		@persona4=Pacientes.new("Arturo",35,1,90,1.79,[],[],[],[],[],[])
 		@persona5=Pacientes.new("Julio",40,1,83,1.70,[],[],[],[],[],[])
 
-		personas = [@persona1, @persona2, @persona3, @persona4, @persona5]
+		@personas = [@persona1, @persona2, @persona3, @persona4, @persona5]
 	
 
 	# PESO TEÓRICO IDEAL:
 	# Convertimos el bloque que calcula el peso teórico ideal en una clase Proc
 	# Para las comprobaciones llamamos al método que convierte los metros en cm e invoca a pt con un parámetro
 	@pt = Proc.new {|x| peso_ideal = (x-150)*0.75+50 }
-	
-	def peso_teorico(tallam)
-		tallacm=tallam*100
-		@pt.call(tallacm)
+
+	def peso_teorico(individuos)
+		 individuos.collect { |nodo| @pt.call(nodo.talla*100) }
 	end
+
 
 	# GASTO ENERGÉTICO BASAL
 	# usamos lambda
- 	
 	@gb_m = -> arg1, arg2, arg3 {((10*arg1)+(6.25*arg2)-(5*arg3)-161).round(2)}
 	@gb_h = -> arg1, arg2, arg3 {((10*arg1)+(6.25*arg2)-(5*arg3)+5).round(2)}
 
-
-	def gasto_basal(edad,peso,tallam,sexo)
-		tallacm=tallam*100
-		if (sexo == 0)
-			@gb_m.call(peso,tallacm,edad)
-		else
-			@gb_h.call(peso,tallacm,edad)
-		
+	def gasto_basal(individuos)
+		@array = individuos.collect {|nodo| if nodo.sexo == 0; @gb_m.call(nodo.peso, nodo.talla*100, nodo.edad) else @gb_h.call(nodo.peso, nodo.talla*100, nodo.edad) end}
+		return @array
 		end
+	
+
+	# EFECTO TERMÓGENO
+	def efecto_termogeno(individuos)
+		@v_ter = gasto_basal(individuos)
+		@v_ter.collect {|nodo| nodo*0.10}
 	end
 	
 
+
+	# GASTO ACTIVIDAD FÍSICA
+
+
+	# GASTO ENERGÉTICO TOTAL
+
+
 	end
-	context"Comprobaciones para PESO TEÓRICO IDEAL" do
-		it "persona1" do	
-			expect(peso_teorico(@persona1.talla)).to eq(70.25)
-		end
-		it "persona2" do	
-			expect(peso_teorico(@persona2.talla)).to eq(57.5)
-		end
-		it "persona5" do
-			expect(peso_teorico(@persona5.talla)).to eq(65)
+	context"Comprobacion para PESO TEÓRICO IDEAL" do
+		it "Vector personas" do	
+			expect(peso_teorico(@personas)).to eq([70.25, 57.5, 56.0, 71.75, 65])
 		end
 	end
 
 	context"Comprobaciones para GASTO ENERGÉTICO BASAL" do
-		it "persona1" do
-			expect(gasto_basal(@persona1.edad,@persona1.peso,@persona1.talla,@persona1.sexo)).to eq(1681.25)
-		end
-		it "persona3" do
-			expect(gasto_basal(@persona3.edad,@persona3.peso,@persona3.talla,@persona3.sexo)).to eq(1181.5)
+		it "Vector personas" do
+			expect(gasto_basal(@personas)).to eq([1681.25, 1399, 1181.5, 1848.75, 1697.5])
 		end
 	end
+
+	context "Comprobaciones para EFECTO TERMÓGENO" do
+		it "Vector personas" do
+			expect(efecto_termogeno(@personas)).to eq([168.125, 139.9, 118.15, 184.875, 169.75])
+		end
+	end
+
 	#it "Comprobaciones para gasto energético basal" do
 	#	expect(@persona1.gasto_energetico(@persona1.sexo,@persona1.peso,@persona1.talla,@persona1.edad)).to eq(597.13)
 	#end
